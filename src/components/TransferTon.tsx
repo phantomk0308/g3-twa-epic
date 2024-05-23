fa6be4f135318095e3cf0a6480aa05782dd2fba4
Whoiskp <khoapham.ptithcm@gmail.com>
2024-05-23 13:32:35 +0700

    update g3.io

--- src/components/TransferTon.tsx
+++ src/components/TransferTon.tsx
@@ -15,7 +15,7 @@ export function TransferTon() {
   return (
     <Card>
       <FlexBoxCol>
-        <h3>Transfer TON</h3>
+        <h3>Transfer TON: G3.io</h3>
         <FlexBoxRow>
           <label>Amount </label>
           <Input

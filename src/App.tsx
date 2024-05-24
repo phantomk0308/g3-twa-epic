ff090e25db126306136f13db3d66b10accaf9729
Whoiskp <khoapham.ptithcm@gmail.com>
2024-05-24 16:05:29 +0700

    Add mint OCC

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { Button, FlexBoxCol, FlexBoxRow } from "./components/styled/styled";
 import { useTonConnect } from "./hooks/useTonConnect";
 import { CHAIN } from "@tonconnect/protocol";
 import "@twa-dev/sdk";
+import { MintOCC } from "./components/MintOCC";
 
 const StyledApp = styled.div`
   background-color: #e8e8e8;
@@ -46,6 +47,7 @@ function App() {
           <Counter />
           <TransferTon />
           <Jetton />
+          <MintOCC />
         </FlexBoxCol>
       </AppContainer>
     </StyledApp>

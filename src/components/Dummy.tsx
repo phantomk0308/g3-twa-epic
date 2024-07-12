7138f988923f879d2e48bc1fb7045600ad61972d
Whoiskp <khoapham.ptithcm@gmail.com>
2024-07-12 09:16:02 +0700

    add dummy validate button

--- src/components/Dummy.tsx
+++ src/components/Dummy.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import { useInitData, retrieveLaunchParams } from "@tma.js/sdk-react";
-import { validate, parse } from "@tma.js/init-data-node";
-import { Flex, Text } from "@radix-ui/themes";
+import { validate } from "@tma.js/init-data-node";
+// import { v as validate } from "../packs/test/entries/index-uL7oQrpu";
+// import { validate } from "../packs/test/entries/node";
+import { Flex, Text, Button } from "@radix-ui/themes";
 
 export function Dummy() {
   const initDataRaw = retrieveLaunchParams();
@@ -9,14 +11,8 @@ export function Dummy() {
 
   const [botToken, setBotToken] = useState<string>("");
 
-  function validate(initDataRaw: any, botToken: string) {
-    try {
-      validate(initDataRaw, botToken);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  }
+  const iniDataRawTest =
+    "user=%7B%22id%22%3A279058397%2C%22first_name%22%3A%22Vladislav%22%2C%22last_name%22%3A%22Kibenko%22%2C%22username%22%3A%22vdkfrost%22%2C%22language_code%22%3A%22en%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=-3788475317572404878&chat_type=private&auth_date=1709144340&hash=371697738012ebd26a111ace4aff23ee265596cd64026c8c3677956a85ca1827";
 
   return (
     <>
@@ -29,12 +25,17 @@ export function Dummy() {
             setBotToken(e.target.value);
           }}
         />
+        <Button
+          onClick={() => {
+            console.log({ iniDataRawTest, botToken });
+            validate(iniDataRawTest, botToken);
+            console.log("validate done");
+          }}
+        >
+          Validate data
+        </Button>
       </Flex>
-      {botToken && (
-        <Flex direction="column" gap="2">
-          <Text>Is Validate: {validate(initDataRaw, botToken)}</Text>
-        </Flex>
-      )}
+
       <Flex direction="column" gap="2">
         <Text>initData</Text>
         <textarea

809ad8093607be5ea4de53243f3e156c18b37f85
Whoiskp <khoapham.ptithcm@gmail.com>
2024-07-14 22:11:24 +0700

    add transfer with bounce and normal

--- src/components/TransferTon.tsx
+++ src/components/TransferTon.tsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import styled from "styled-components";
-import { Address, toNano, comment } from "ton";
+import { Address, toNano, comment, beginCell } from "ton";
 import { useTonConnect } from "../hooks/useTonConnect";
 import { Card, FlexBoxCol, FlexBoxRow, Button, Input } from "./styled/styled";
+import { useTonConnectUI } from "@tonconnect/ui-react";
 
 export function TransferTon() {
   const { sender, connected } = useTonConnect();
+  const [tonConnectUI] = useTonConnectUI();
 
-  const [tonAmount, setTonAmount] = useState("0.001");
-  const [tonComment, setTonComment] = useState("");
+  const [tonAmount, setTonAmount] = useState("0.02");
+  const [tonComment, setTonComment] = useState("Check in");
   const [tonRecipient, setTonRecipient] = useState(
-    "UQCqOu6ajaBgdcvlVhmU3WaD2d7HQaCzRNdn8xcJx31kbvkr"
+    "UQCTSiULauZbMap-b9sYoSgWMHZPGF4YPG4cTp5Nepb3Hfuf"
   );
 
   return (
@@ -49,11 +51,38 @@ export function TransferTon() {
             sender.send({
               to: Address.parse(tonRecipient),
               value: toNano(tonAmount),
+              // bounce: true,
               body: comment(tonComment),
             });
           }}
         >
-          Transfer
+          Transfer Only Pay GAS
+        </Button>
+        <Button
+          disabled={!connected}
+          style={{ marginTop: 18 }}
+          onClick={async () => {
+            const nonce = Math.floor(10000 + Math.random() * 100000);
+            const memo_tx = `MINT $user_id $package_id ${nonce}`;
+
+            const comment = beginCell()
+              .storeUint(0, 32)
+              .storeStringTail(memo_tx)
+              .endCell();
+
+            await tonConnectUI.sendTransaction({
+              validUntil: Math.floor(Date.now() / 1000) + 360,
+              messages: [
+                {
+                  address: tonRecipient,
+                  amount: toNano(tonAmount).toString(),
+                  payload: comment.toBoc().toString("base64"),
+                },
+              ],
+            });
+          }}
+        >
+          Transfer Normal Wallet
         </Button>
       </FlexBoxCol>
     </Card>
